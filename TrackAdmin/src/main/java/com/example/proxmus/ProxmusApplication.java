64adffbe00295c71e2e47185f0a81893bdae719b
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2023-07-25 15:57:08 +0200

    Add files via upload

---
+++ TrackAdmin/src/main/java/com/example/proxmus/ProxmusApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxmus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxmusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxmusApplication.class, args);
+    }
+
+}
---
+++ TrackAdmin/src/test/java/com/example/proxmus/ProxmusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proxmus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxmusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
