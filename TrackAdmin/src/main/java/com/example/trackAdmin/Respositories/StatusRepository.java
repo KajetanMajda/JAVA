1839a8a5c2967197e2037a46c8330feea3f18015
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2023-08-03 15:36:22 +0200

    Delete TrackAdmin directory

--- TrackAdmin/src/main/java/com/example/trackAdmin/Respositories/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.trackAdmin.Respositories;
-
-import com.example.trackAdmin.Classes.Status;
-import com.example.trackAdmin.Classes.Users;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StatusRepository extends CrudRepository<Status, Integer> {
-    Status findByName(String name);
-}
--- TrackAdmin/src/main/java/com/example/trackAdmin/Respositories/UsersRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.trackAdmin.Respositories;
-
-import com.example.trackAdmin.Classes.UsersRole;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersRoleRepository extends CrudRepository<UsersRole, Integer> {
-    UsersRole findByName(String roleName);
-}
