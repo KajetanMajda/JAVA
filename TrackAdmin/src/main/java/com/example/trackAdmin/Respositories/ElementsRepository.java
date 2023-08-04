06111426c32fb0d592aa2f53506f33e3400474fe
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2023-08-04 15:53:55 +0200

    Delete TrackAdmin directory

--- TrackAdmin/src/main/java/com/example/trackAdmin/Respositories/ElementsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.trackAdmin.Respositories;
-
-import com.example.trackAdmin.Classes.Elements;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ElementsRepository extends CrudRepository<Elements, Integer> {
-    Elements findByLp(Integer lp);
-}
