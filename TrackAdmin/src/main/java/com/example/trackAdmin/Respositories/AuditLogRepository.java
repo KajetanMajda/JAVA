4f4d727fedbdb3469b5c17d5fa042a8d1b965a6d
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2023-08-09 11:28:50 +0200

    Delete TrackAdmin directory

--- TrackAdmin/src/main/java/com/example/trackAdmin/Respositories/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.trackAdmin.Respositories;
-
-import com.example.trackAdmin.Classes.AuditLog;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuditLogRepository extends CrudRepository<AuditLog, Long> {
-}
